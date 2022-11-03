8393af3d668d9a4315af3593c5d3dd5ab0eb10e0
initialhaa <initialhaa@gmail.com>
2022-11-03 10:24:42 +0700

    first commit

---
+++ variabel.js
@@ -0,0 +1,14 @@
+// membuat variabel
+var name = "Kelompok 7";
+var NamaAnggota = "Hanafi, Intan, Ayu, Okta";
+var isActive = true;
+var url = "Pamulang";
+
+// menampilkan variabel ke jendela dialog (alert)
+alert("Selamat datang di " + name);
+
+// menampilkan variabel ke dalam HTML
+document.write("Nama Kelompok : " + name + "<br>");
+document.write("Nama Anggota : " + NamaAnggota + "<br>");
+document.write("Status Aktif  : " + isActive + "<br>");
+document.write("Alamat URL    : " + url + "<br>")
